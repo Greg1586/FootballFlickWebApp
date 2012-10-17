ab821c26f514613259fc4066cd9aa4653e7e4179
Ste <scowley@thoughtworks.com>
2012-10-17 21:34:11 +0100

    Ste|Mansi: set up application and created db

---
+++ footballflick/app/controllers/homes_controller.rb
@@ -0,0 +1,29 @@
+class HomesController < ApplicationController
+  def index
+    @homes = Home.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ footballflick/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footballflick::Application.config.secret_token = 'b82ddfedd1bdb0f67b94254ddb11635d4f29df648c4c3dbd7aa2f9ea0cbe6fabe0011165d4ea8ce15917ca11129b30bde16a8dbcb51d8c912bd6fc328f5f5816'
