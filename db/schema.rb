460a1c15a64f0ad545b56455d91716eb3ecfacd9
Caroline <Caroline@TW>
2012-10-29 12:27:19 +0000

    Caroline|Mansi - integrating get skeleton and jquery mobile.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +15,7 @@ ActiveRecord::Schema.define(:version => 20121023161331) do
 
   create_table "homes", :force => true do |t|
     t.string   "title"
-    t.binary   "icon"
+    t.string   "icon"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
