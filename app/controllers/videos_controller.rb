875f82517af9ad762e116b1a958397663b6c7138
maryamaidini <maidini@thoughtworks.com>
2012-10-31 11:21:55 +0000

    Added some tests for Vide controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
+    @videos = Video.find(:all)
   end
 
   def show
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -5,4 +5,17 @@ describe VideosController do
     get :index
     response.should render_template 'index'
   end
+
+  it  'should show video that has been selected' do
+    video=Video.new
+    Video.should_receive(:find).with("1").and_return(video)
+    get :show ,{:id => 1}
+  end
+
+  it 'should show all the videos in the database' do
+    video=Video.new
+    Video.should_receive(:find).with(:all).and_return([video])
+    get :index
+    assigns(:videos).should match_array([video])
+  end
 end
