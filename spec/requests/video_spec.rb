ddbf0a7ff5ff541a0959a96e1cc2f05605d9301c
maryamaidini <maidini@thoughtworks.com>
2012-10-31 16:06:13 +0000

    Add test for show video page

--- spec/requests/video_spec.rb
+++ spec/requests/video_spec.rb
@@ -5,10 +5,18 @@ describe "videos" do
 
   describe "GET/videos" do
 
-    it "should display videos that have been already selected" do
+    it "should display right title on video page" do
       visit('/videos')
       page.should have_content("Football Flick")
+    end
+
+    it "should display video that have been already selected in show page" do
+      Video.create!(:icon => "icon", :title  => "title", :description => "description")
+      visit('/videos/1')
+      page.should have_content("description")
+      page.should have_content("mediaplayer")
     end
   end
 
 end
+
