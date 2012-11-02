c8be2b803655a3afe2b48f8aecc3817770ba8ed7
Caroline <Caroline@TW>
2012-11-02 12:05:06 +0000

    Caroline+Ste: fixed tests

--- spec/requests/video_spec.rb
+++ spec/requests/video_spec.rb
@@ -7,14 +7,14 @@ describe "videos" do
 
     it "should display right title on video page" do
       visit('/videos')
-      page.should have_content("Football Flick")
+      page.should have_selector("img", :src => 'assets/HeaderLogo.png')
     end
 
-    it "should display video that have been already selected in show page" do
+    it "should display video on individual video page" do
       Video.create!(:icon => "icon", :title  => "title", :description => "description")
       visit('/videos/1')
       page.should have_content("description")
-      page.should have_content("mediaplayer")
+      page.should have_content("video")
     end
   end
 
