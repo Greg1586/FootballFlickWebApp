86b47c4d2e804640a1d3f132bb459af7cabbffed
Ste <cowley05@hotmail.co.uk>
2012-11-13 14:33:18 +0000

    Maryam|Ste: Added descriptions from live clicker

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,20 @@
 class VideosController < ApplicationController
-
+include HTTParty
   def index
     @videos = Video.find(:all)
   end
 
   def show
+    widget_id = params[:id]
+    logger.info("="*100)
+    logger.info(widget_id)
+    logger.info("="*100)
+    response = HTTParty.get('http://api.liveclicker.com/service/api?method=liveclicker.widget.getInfo&account_id=1584&format=json&widget_id='+widget_id)
+    widgetinfo = JSON(response)
 
+    widgetinfojson = JSON.parse(widgetinfo)
+    widget = widgetinfojson["widget"]
+    @description = widget["metadescription"]
   end
 
 end
