4bbdc9234ddd22d68935cf3a120007d0ab0aa542
maryamaidini <maidini@thoughtworks.com>
2012-11-15 17:05:29 +0000

    Maryam - Add test for Show page

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -6,7 +6,11 @@ describe VideosController do
     response.should render_template 'index'
   end
 
-
+  it 'should render show page ' do
+    visit('/videos')
+    get :show ,{:id => 1661360432}
+    assigns(:description).should eq("This is the video number 10.")
+  end
 
 
 end
