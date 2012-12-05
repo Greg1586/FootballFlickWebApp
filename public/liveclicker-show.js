1c0cf6d3c36ba02d5f967c0fa28b3509c19831f7
mkulkarn <mkulkarn@thoughtworks.com>
2012-12-05 23:00:43 +0000

    Manasi - fixed votw problem

--- public/liveclicker-show.js
+++ public/liveclicker-show.js
@@ -1,18 +1,34 @@
-if(document.getElementById("description-box")){
 lc.settings = { 'account_id':1584 };
-var path = window.location.pathname;
-path = path.substring(8);
 
-$(document).ready(function () {
-    player();
-});
+if(document.getElementById("description-box")){
 
-function player() {
-    lc({'widget_id':path}).isReady(function () {
-        x = document.getElementById("videos-page");
-        width = (x.offsetWidth * 0.95);
-        height = (width * 0.57);
-        lc(this).getByIndex(0).getPlayer({'player_id':1285, 'width':width, 'height':height}).appendPlayerTo({'id':'show-videos'});
+    var path = window.location.pathname;
+    path = path.substring(8);
+
+    $(document).ready(function () {
+        playerStart();
     });
+
+    function playerStart() {
+        lc({'widget_id':path}).isReady(function () {
+            x = document.getElementById("videos-page");
+            width = (x.offsetWidth * 0.95);
+            height = (width * 0.57);
+            lc(this).getByIndex(0).getPlayer({'player_id':1285, 'width':width, 'height':height}).appendPlayerTo({'id':'show-videos'});
+        });
+    }
 }
+
+if(document.getElementById("video-of-week")){
+    $(document).ready(function () {
+        player();
+    });
+    function player() {
+        lc({'tag':'VideoOfTheWeek'}).isReady(function () {
+            x = document.getElementById("video-of-week");
+            width = (x.offsetWidth * 0.95);
+            height = (width * 0.57);
+            lc(this).getByIndex(0).getPlayer({'player_id':1285, 'width':width, 'height':height}).appendPlayerTo({'id':'VideoOfTheWeek'});
+        });
+    }
 }
